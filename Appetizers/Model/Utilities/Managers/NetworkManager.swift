e0319596388b87171d018dd79fa9f390928d733b
temoorx <taak140@gmail.com>
2024-04-27 18:58:34 +0500

    project structure and remote image setup

--- Appetizers/Model/Utilities/Managers/NetworkManager.swift
+++ Appetizers/Model/Utilities/Managers/NetworkManager.swift
@@ -63,35 +63,34 @@ final class NetworkManager {
             throw APError.invalidData
         }
     }
-
-//    
+   
     
-//    func downloadImage(fromURLString urlString: String, completed: @escaping (UIImage?) -> Void ) {
-//        
-//        let cacheKey = NSString(string: urlString)
-//        
-//        if let image = cache.object(forKey: cacheKey) {
-//            completed(image)
-//            return
-//        }
-//        
-//        guard let url = URL(string: urlString) else {
-//            completed(nil)
-//            return
-//        }
-//        
-//        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
-//            
-//            guard let data, let image = UIImage(data: data) else {
-//                completed(nil)
-//                return
-//            }
-//            
-//            self.cache.setObject(image, forKey: cacheKey)
-//            completed(image)
-//        }
-//        
-//        task.resume()
-//    }
+    func downloadImage(fromURLString urlString: String, completed: @escaping (UIImage?) -> Void ) {
+        
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            
+            guard let data, let image = UIImage(data: data) else {
+                completed(nil)
+                return
+            }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
+        }
+        
+        task.resume()
+    }
 }
 
