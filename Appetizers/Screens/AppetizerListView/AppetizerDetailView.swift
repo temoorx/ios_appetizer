75965ebb93ad975f90b650873ad565941402e5d4
temoorx <taak140@gmail.com>
2024-04-27 19:49:59 +0500

    detail view setup

--- Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -8,11 +8,91 @@
 import SwiftUI
 
 struct AppetizerDetailView: View {
+    
+    let appetizer: Appetizer
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image("asian-flank")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 225)
+            VStack{
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                Text(appetizer.description)
+                    .multilineTextAlignment(.center)
+                    .font(.body)
+                    .padding()
+                HStack(spacing: 40){
+                    VStack(spacing: 5){
+                        Text("Calories")
+                            .bold()
+                            .font(.caption)
+                        Text("\(appetizer.calories)")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    VStack(spacing: 5){
+                        Text("Carbs")
+                            .bold()
+                            .font(.caption)
+                        Text("\(appetizer.carbs)")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    VStack(spacing: 5){
+                        Text("Protein")
+                            .bold()
+                            .font(.caption)
+                        Text("\(appetizer.protein)")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+
+
+                }
+            }
+            
+            Spacer()
+            Button{
+                print("tapped")
+            }label: {
+                Text("$\(appetizer.price, specifier: "%.2f") - Add to Order")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .frame(width: 260, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.primaryColor)
+                    .cornerRadius(10)
+                
+            }.padding(.bottom, 30)
+        }
+        .frame(width: 300, height: 525)
+        .background(Color(.systemBackground))
+        .cornerRadius(12)
+        .shadow(radius: 40)
+        .overlay(Button{
+            print("dismissed")
+        }label: {
+            ZStack{
+                Circle()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.white)
+                    .opacity(0.6)
+                Image(systemName: "xmark")
+                    .imageScale(.small)
+                    .frame(width: 44, height: 44)
+                    .foregroundColor(.black)
+            }
+        },
+        alignment: .topTrailing)
     }
 }
 
 #Preview {
-    AppetizerDetailView()
+    AppetizerDetailView(appetizer: MockData.sampleAppetizer)
 }
--- Appetizers/Views/UIKit Components/LoadingView.swift
+++ Appetizers/Views/UIKit Components/LoadingView.swift
@@ -7,28 +7,31 @@
 
 import SwiftUI
 
-struct ActivityIndicator : UIViewRepresentable{
-    
-    func makeUIView(context: Context) -> UIActivityIndicatorView {
-        
-        let activityIndicatorView = UIActivityIndicatorView(style: .large)
-        activityIndicatorView.color = UIColor.primaryColor
-        activityIndicatorView.startAnimating()
-        return activityIndicatorView
-        
-    }
-    
-    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
-    }
-}
+//struct ActivityIndicator : UIViewRepresentable{
+//    
+//    func makeUIView(context: Context) -> UIActivityIndicatorView {
+//        
+//        let activityIndicatorView = UIActivityIndicatorView(style: .large)
+//        activityIndicatorView.color = UIColor.primaryColor
+//        activityIndicatorView.startAnimating()
+//        return activityIndicatorView
+//        
+//    }
+//    
+//    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
+//    }
+//}
 
 struct LoadingView : View {
     var body: some View {
         ZStack{
             Color(.systemBackground)
                 .edgesIgnoringSafeArea(.all)
+                
             
-            ActivityIndicator()
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle(tint: .primaryColor))
+                .scaleEffect(2.0, anchor: .center)
         }
     }
 }
