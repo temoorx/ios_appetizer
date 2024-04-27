d1e232b718f9b413618db5da395932a44c6c507c
temoorx <taak140@gmail.com>
2024-04-27 19:49:36 +0500

    detail view setup

---
+++ Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  AppetizerDetailView.swift
+//  Appetizers
+//
+//  Created by Mac on 27/04/2024.
+//
+
+import SwiftUI
+
+struct AppetizerDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppetizerDetailView()
+}
